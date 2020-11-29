637bd8986f3ee601e094815994cdea42159d8244
まさとし <>
2020-11-30 00:56:09 +0900

    トップページ編集

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,19 +26,24 @@
 		</a>
 	</div>
 </div>
-<style>
+
+<h1>商品を探す</h1>
+
+<div class="cat-grid">
+	@for($i = 0; $i < 10; $i++)
+		<div class="card">
+			<span data-toggle="modal" data-target="#staticBackdrop{{ $i }}">
+				<img src="https://picsum.photos/id/{{ rand(1, 10) * $i }}/500/250" class="card-img" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+				</div>
+			</span>
+		</div>
+	@endfor
+	<div class="bg-dark text-white"><a href="">商品カテゴリ一覧</a></div>
+</div>
 
 
-</style>
-
-<ul class="nav nav-tabs" id="informationTab" role="tablist">
-	<li class="nav-item" role="presentation">
-		<a class="nav-link active" id="pickup-tab" data-toggle="tab" href="#pickup" role="tab" aria-controls="pickup" aria-selected="true">新着情報</a>
-	</li>
-	<li class="nav-item" role="presentation">
-		<a class="nav-link" id="category-tab" data-toggle="tab" href="#category" role="tab" aria-controls="category" aria-selected="false">商品を探す</a>
-	</li>
-</ul>
 <style>
 	.grid,
 	.cat-grid {
@@ -84,39 +89,26 @@
 	}
 
 </style>
-<div class="tab-content bg-light" id="informationTabContent">
+{{-- <div class="tab-content bg-light" id="informationTabContent">
 	<div class="tab-pane fade show active p-3" id="pickup" role="tabpanel" aria-labelledby="pickup-tab">
 		<div class="grid">
-			@for($i = 0; $i < 10; $i++)
-				<a href="{{ route('detail') }}" class="grid__inner grid__inner--{{ $i % 5 + 1 }} bg-white">
-					<div class="image">
-						<img class="" src="https://picsum.photos/id/{{ rand(1, 100) * $i+ 1 }}/585/365" alt="...">
-					</div>
-					<div class="card-body">
-						<p>Lorem ipsum dolor sit amet.</p>
-						<h1 class="">Card title</h1>
-					</div>
-				</a>
-			@endfor
-		</div>
-	</div>
-	<div class="tab-pane fade p-3" id="category" role="tabpanel" aria-labelledby="category-tab">
-		<div class="cat-grid">
-			@for($i = 0; $i < 10; $i++)
-				<div class="card">
-					<span data-toggle="modal" data-target="#staticBackdrop{{ $i }}">
-						<img src="https://picsum.photos/id/{{ rand(1, 10) * $i }}/500/250" class="card-img" alt="...">
-						<div class="card-body">
-							<h5 class="card-title">Card title</h5>
-						</div>
-					</span>
-				</div>
-			@endfor
-			<div class="bg-dark text-white"><a href="">商品カテゴリ一覧</a></div>
-		</div>
-	</div>
+@for($i = 0; $i < 10; $i++) <a href="{{ route('detail') }}" class="grid__inner grid__inner--{{ $i % 5 + 1 }} bg-white">
+				<div class="image">
+					<img class="" src="https://picsum.photos/id/{{ rand(1, 100) * $i+ 1 }}/585/365" alt="...">
+</div>
+<div class="card-body">
+	<p>Lorem ipsum dolor sit amet.</p>
+	<h1 class="">Card title</h1>
+</div>
+</a>
+@endfor
+</div>
+</div>
+<div class="tab-pane fade p-3" id="category" role="tabpanel" aria-labelledby="category-tab">
 
 </div>
+
+</div> --}}
 
 
 
@@ -124,39 +116,22 @@
 	<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
 		<div class="modal-content">
 			<div class="modal-header">
-				<ul class="nav nav-tabs" id="myTab" role="tablist">
-					<li class="nav-item" role="presentation">
-						<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Sub-Category1</a>
-					</li>
-					<li class="nav-item" role="presentation">
-						<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Sub-Category2</a>
-					</li>
-					<li class="nav-item" role="presentation">
-						<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Sub-Category3</a>
-					</li>
-				</ul>
+				Category 1
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
 			<div class="modal-body">
-				<div class="tab-content" id="myTabContent">
-					<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-						<div class="cat-grid">
-
-							@for($i = 0; $i < 6; $i++)
-								<div class="card">
-									<a href="{{ route('list') }}">
-										<img src="https://picsum.photos/id/{{ rand(1, 10) * $i }}/500/250" class="card-img" alt="...">
-										<div class="card-body">
-											<h5 class="card-title">Card title</h5>
-										</div>
-									</a>
-								</div>
-							@endfor
-						</div>
-					</div>
+				<div class="cat-grid">
+					@for($i = 0; $i < 6; $i++) <div class="card">
+						<a href="{{ route('list') }}">
+							<img src="https://picsum.photos/id/{{ rand(1, 10) * $i }}/500/250" class="card-img" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+							</div>
+						</a>
 				</div>
+				@endfor
 			</div>
 		</div>
 	</div>
