7cf22c3c4033c686f0765f3a6678f2ce3f4adb08
まさとし <>
2021-01-22 01:25:16 +0900

    [UPDATE] Compile assets files for production.

--- public/js/hero_anime.js
+++ public/js/hero_anime.js
@@ -1,28 +1,2 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/*!************************************!*\
-  !*** ./resources/js/hero_anime.js ***!
-  \************************************/
-
-
-var heroTl = gsap.timeline({
-  defaults: {
-    duration: .7,
-    ease: 'power3.out'
-  }
-});
-heroTl.from('.layered-image', {
-  scale: 1.3,
-  transformOrigin: '50% 50%',
-  filter: 'grayscale(100%)',
-  delay: .2
-});
-gsap.utils.toArray(['.layered-text', '.layered-title', '.layered-desc', '.layered-button']).forEach(function (elm) {
-  heroTl.from(elm, {
-    autoAlpha: 0,
-    y: 150
-  });
-});
-/******/ })()
-;
+(()=>{"use strict";var e=gsap.timeline({defaults:{duration:.7,ease:"power3.out"}});e.from(".layered-image",{scale:1.3,transformOrigin:"50% 50%",filter:"grayscale(100%)",delay:.2}),gsap.utils.toArray([".layered-text",".layered-title",".layered-desc",".layered-button"]).forEach((function(a){e.from(a,{autoAlpha:0,y:150})}))})();
 //# sourceMappingURL=hero_anime.js.map
